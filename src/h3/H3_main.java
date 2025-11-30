aa03c141cd7f961667880afdf1f2f0a5a00cffc6
Son <son.trinhh07@gmail.com>
2025-11-30 22:05:40 +0100

    3. Hausaufgabe

--- src/h3/H3_main.java
+++ src/h3/H3_main.java
@@ -3,21 +3,37 @@ package h3;
 public class H3_main {
 
 	public static void main(String[] args) {
-		System.out.println(compareArraysVal(new int [] {2,3,4,5}, new int[] {3,2,4,5}));
 		
-
 	}
-	public static boolean compareArraysVal(int [] a, int [] b) {
-		boolean ergebnis = false;
-		
-		if (a.length == b.length) {
-			for (int x = 0; x < a.length; x++) {
-			for (int y = 0; x < a.length; x++);
-			
-			
-			
-			
-		}
-	}
+	public static boolean compareArraysVal(int[] a, int[] b) {
+	   
+	    if (a.length != b.length) {
+	        return false;
+	    }
 
-	}}
+	    
+	    for (int i = 0; i < a.length; i++) {
+	        int countA = 0;
+	        int countB = 0;
+
+	        
+	        for (int j = 0; j < a.length; j++) {
+	            if (a[j] == a[i]) {
+	                countA++;
+	            }
+	        }
+
+	        for (int j = 0; j < b.length; j++) {
+	            if (b[j] == a[i]) {
+	                countB++;
+	            }
+	        }
+	        if (countA != countB) {
+	            return false;
+	        }
+	    }
+
+	    
+	    return true;
+	}
+}
